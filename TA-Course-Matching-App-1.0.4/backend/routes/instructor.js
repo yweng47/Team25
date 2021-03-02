a38f6f472ef9229bd0e8a77e0ec828ba65c0060f
yzha2776 <62304919+yzha2776@users.noreply.github.com>
2021-03-02 18:23:56 -0500

    show download course and test question
    Add the functions that ECE department can show the download course and test questions now

---
+++ TA-Course-Matching-App-1.0.4/backend/routes/instructor.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../schema/user');
+const Course = require('../schema/course');
+const Question = require('../schema/question');
+const {genSuccessResponse} = require('../utils/utils');
+const mongoose = require('mongoose');
+const {genErrorResponse} = require('../utils/utils');
+const {genInvalidParamsResponse} = require('../utils/utils');
+const ObjectId = mongoose.Types.ObjectId;
+
+router.get('/course', async function(req, res, next) {
+	const { id } = req.query;
+
+	if (!id) {
+		return res.join(genInvalidParamsResponse());
+	}
+
+	let courses = await User.aggregate([
+		{
+			$lookup: {
+				from: "courses",
+				localField: "relateCourses",
+				foreignField: "_id",
+				as: "courses"
+			}
+		},
+		{
+			$match:{
+				'_id': ObjectId(id)
+			}
+		},
+		{
+			$project: {
+				"courses": 1,
+				"_id": 0
+			}
+		},
+		{
+			$limit: 1
+		}
+	]).exec();
+	return res.json(genSuccessResponse(courses[0].courses));
+});
+
+router.get('/course/:id', async function(req, res, next) {
+	const { id } = req.params;
+
+	if (!id) {
+		return res.join(genInvalidParamsResponse());
+	}
+
+	const course = await Course.findOne({ _id: id }).exec();
+	return res.json(genSuccessResponse(course));
+});
+
+router.put('/course', async function(req, res, next) {
+	const courseBody = req.body;
+
+	if (!courseBody) {
+		return res.join(genInvalidParamsResponse());
+	}
+
+	const course = new Course({ ...courseBody })
+
+	Course.findOneAndUpdate({ _id: course._id }, course, (err) => {
+		if (err) {
+			res.json(genErrorResponse(err));
+		} else {
+			res.json(genSuccessResponse());
+		}
+	});
+});
+
+router.get('/question', async function(req, res, next) {
+	const { course, user } = req.query;
+
+	const match = {};
+	if (course) {
+		match.course = ObjectId(course);
+	}
+	if (user) {
+		match.user = ObjectId(user);
+	}
+
+	const questions = await Question.aggregate([
+		{
+			$lookup: {
+				from: "courses",
+				localField: "course",
+				foreignField: "_id",
+				as: "courses"
+			}
+		},
+		{
+			$lookup: {
+				from: "users",
+				localField: "user",
+				foreignField: "_id",
+				as: "users"
+			}
+		},
+		{
+			$match: match
+		}
+	]).exec();
+
+	return res.json(genSuccessResponse(questions));
+});
+
+router.post('/question', async function(req, res, next) {
+	const questionBody = req.body;
+
+	if (!questionBody) {
+		return res.join(genInvalidParamsResponse());
+	}
+
+	const question = new Question({ ...questionBody })
+
+	Question.findOneAndUpdate({ _id: question._id }, question, { upsert: true }, (err) => {
+		if (err) {
+			res.json(genErrorResponse(err));
+		} else {
+			res.json(genSuccessResponse());
+		}
+	});
+});
+
+module.exports = router;
---
+++ TA-Course-Matching-App-1.0.4/frontend/src/app/config/role.ts
@@ -0,0 +1,5 @@
+export const ROLE = {
+  ADMIN: 'admin',
+  INSTRUCTOR: 'instructor',
+  CHAIR: 'chair'
+};
